c8b79f4c8b97772102ced9094a95be32fcd7a8cb
gale31 <gale.staneva@gmail.com>
2017-04-25 02:14:59 +0300

    ieeg portal commit

---
+++ kaggle/approach00/generate_features.m
@@ -0,0 +1,41 @@
+function generate_features(dataDir, featureDir, trainortest)
+% This function generates a set of features which are later used to train a model.
+% Here the features are simply the average for the whole signal in each channel.
+
+% Read data, compute and save features
+% Warning: I have downloaded only train_1 and train_2, so these are the
+% only data files I'm going to extract features from.
+
+for_test = [];
+for i = 1:2
+    trainingdir = fullfile(dataDir, strcat(trainortest, num2str(i)));
+    dirv = dir([trainingdir, '/*.mat']);
+    
+    j = 0;
+    for samplefile = dirv'
+        
+        split = strsplit(samplefile.name,'_');
+        for_test = [for_test; strcat(split(2), '_', split(3))];
+      
+        j = j + 1;
+         
+        sample = load(fullfile(trainingdir, samplefile.name));
+        
+        average = zeros(1, 16);
+        
+        for channel = 1:16
+            chanarr = sample.dataStruct.data(:, channel);
+            average(1, channel) = mean(chanarr);
+        end
+        
+        savedir = fullfile(featureDir, strcat(trainortest, num2str(i), '_', num2str(j), '.mat'));
+       
+        display(savedir);
+        save(savedir, 'average');
+    end
+end
+
+savedir = fullfile(featureDir, '/for_test.mat');
+save(savedir, 'for_test');
+
+end
---
+++ kaggle/approach02/workflow.m
@@ -0,0 +1,206 @@
+close all; clear;  clc;
+
+dataDir = '/Users/Gale/Documents/data';
+featureDir = '/Users/Gale/Documents/Seizure-Prediction/features';
+
+if ~exist(dataDir,'dir')
+    mkdir(dataDir)
+elseif ~exist(featureDir,'dir')
+    mkdir(featureDir)
+end
+%% Compute a set of features for the training (and test) data.
+
+% We will compute this set of features for every one of the two (or three, or n) patients
+
+dirb = 'train_'; patient = '1';
+generate_features(dataDir, featureDir, dirb, patient);
+ 
+dirb = 'train_'; patient = '2';
+generate_features(dataDir, featureDir, dirb, patient);
+
+dirb = 'train_'; patient = '3';
+generate_features(dataDir, featureDir, dirb, patient);
+
+%% Prepare Y
+  
+ Y = [];
+ 
+ fileOrder = load(strcat(featureDir, '/fileOrder_train_1.mat'));
+ 
+ for i = 1:720
+     
+     split = strsplit(char(fileOrder.fileOrder(i, 1)),'_');
+     
+     if char(split(2)) == '0.mat'
+         Y = [Y; 0];
+     else
+         Y = [Y; 1];
+     end
+     
+ end
+ 
+ savedir = fullfile(featureDir, '/Y_1.mat');
+ save(savedir, 'Y')
+  
+ Y = []
+ 
+ fileOrder = load(strcat(featureDir, '/fileOrder_train_2.mat'));
+ 
+ for i = 1:1986
+     
+     split = strsplit(char(fileOrder.fileOrder(i, 1)),'_');
+     
+     if char(split(2)) == '0.mat'
+         Y = [Y; 0];
+     else
+         Y = [Y; 1];
+     end
+     
+ end
+ 
+ savedir = fullfile(featureDir, '/Y_2.mat');
+ save(savedir, 'Y');
+
+Y = [];
+ 
+fileOrder = load(strcat(featureDir, '/fileOrder_train_3.mat'));
+ 
+for i = 1:2058
+    split = strsplit(char(fileOrder.fileOrder(i, 1)),'_');
+     
+    if char(split(2)) == '0.mat'
+        Y = [Y; 0];
+    else
+        Y = [Y; 1];
+    end 
+end
+ 
+savedir = fullfile(featureDir, '/Y_3.mat');
+save(savedir, 'Y');
+
+%% Train a model based on the data.
+
+% For the first patient.
+
+average_1 = load('/Users/Gale/Documents/Seizure-Prediction/features/average_train_1.mat');
+std_1 = load('/Users/Gale/Documents/Seizure-Prediction/features/std_train_1.mat');
+
+patient1x = zeros(720, 2, 16);
+for i = 1:16
+    patient1x(:, :, i) = [average_1.average(:, i) std_1.standardDev(:, i)];
+end
+
+Y_1 = load('/Users/Gale/Documents/Seizure-Prediction/features/Y_1.mat');
+
+patient1dtree = cell(16, 1);
+for i = 1:16  
+    patient1dtree{i} = fitctree(patient1x(:, :, i), Y_1.Y);
+end
+
+% view(patient1dtree{3},'mode','graph')
+
+% For the second patient.
+
+average_2 = load('/Users/Gale/Documents/Seizure-Prediction/features/average_train_2.mat');
+std_2 = load('/Users/Gale/Documents/Seizure-Prediction/features/std_train_2.mat');
+
+patient2x = zeros(1986, 2, 16);
+for i = 1:16
+    patient2x(:, :, i) = [average_2.average(:, i) std_2.standardDev(:, i)];
+end
+
+Y_2 = load('/Users/Gale/Documents/Seizure-Prediction/features/Y_2.mat');
+
+patient2dtree = cell(16, 1);
+for i = 1:16  
+    patient2dtree{i} = fitctree(patient2x(:, :, i), Y_2.Y);
+end
+
+% view(patient1dtree{3},'mode','graph');
+
+% And, finally, for the third patient.
+
+average_3 = load('/Users/Gale/Documents/Seizure-Prediction/features/average_train_3.mat');
+std_3 = load('/Users/Gale/Documents/Seizure-Prediction/features/std_train_3.mat');
+
+patient3x = zeros(2058, 2, 16);
+for i = 1:16
+    patient3x(:, :, i) = [average_3.average(:, i) std_3.standardDev(:, i)];
+end
+
+Y_3 = load('/Users/Gale/Documents/Seizure-Prediction/features/Y_3.mat');
+
+patient3dtree = cell(16, 1);
+for i = 1:16  
+    patient3dtree{i} = fitctree(patient3x(:, :, i), Y_3.Y);
+end
+
+view(patient3dtree{3},'mode','graph');
+
+%% Predict for the test data.
+
+% We'll have to generate features again
+
+dirb = 'test_'; patient = '1';
+generate_features(dataDir, featureDir, dirb, patient);
+
+dirb = 'test_'; patient = '2';
+generate_features(dataDir, featureDir, dirb, patient);
+
+dirb = 'test_'; patient = '3';
+generate_features(dataDir, featureDir, dirb, patient);
+
+average_1 = load('/Users/Gale/Documents/Seizure-Prediction/features/average_test_1.mat');
+std_1 = load('/Users/Gale/Documents/Seizure-Prediction/features/std_test_1.mat');
+  
+Y = zeros(216, 16);
+for i = 1:16
+    X = [average_1.average(:, i) std_1.standardDev(:, i)];
+    Y(:, i) = predict(patient1dtree{i}, X);
+end
+
+prediction_1 = mean(Y.');
+
+average_2 = load('/Users/Gale/Documents/Seizure-Prediction/features/average_test_2.mat');
+std_2 = load('/Users/Gale/Documents/Seizure-Prediction/features/std_test_2.mat');
+  
+Y = zeros(1002, 16);
+for i = 1:16
+    X = [average_2.average(:, i) std_2.standardDev(:, i)];
+    Y(:, i) = predict(patient2dtree{i}, X);
+end
+
+prediction_2 = mean(Y.');
+
+average_3 = load('/Users/Gale/Documents/Seizure-Prediction/features/average_test_3.mat');
+std_3 = load('/Users/Gale/Documents/Seizure-Prediction/features/std_test_3.mat');
+  
+Y = zeros(690, 16);
+for i = 1:16
+    X = [average_3.average(:, i) std_3.standardDev(:, i)];
+    Y(:, i) = predict(patient3dtree{i}, X);
+end
+
+prediction_3 = mean(Y.');
+
+fileID = fopen('/Users/Gale/Documents/Seizure-Prediction/approach01/submission.csv','wt');
+
+fileOrder_1 = load('/Users/Gale/Documents/Seizure-Prediction/features/fileOrder_test_1.mat');
+fileOrder_2 = load('/Users/Gale/Documents/Seizure-Prediction/features/fileOrder_test_2.mat');
+fileOrder_3 = load('/Users/Gale/Documents/Seizure-Prediction/features/fileOrder_test_3.mat');
+
+display('Writing to csv...');
+
+for i = 1:1908
+    if i <= 216
+        fprintf(fileID, '%s,%f\n', char(fileOrder_1.fileOrder(i)), double(prediction_1(i)));
+    elseif i <= 1218
+        fprintf(fileID, '%s,%f\n', char(fileOrder_2.fileOrder(i-216)), double(prediction_2(i-216)));
+    else
+        fprintf(fileID, '%s,%f\n', char(fileOrder_3.fileOrder(i-1218)), double(prediction_3(i-1218)));
+    end
+end
+
+display('Mission accomplished.');
+
+fclose(fileID);
